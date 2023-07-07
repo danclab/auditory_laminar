86a52b6c419deb599ddb153cd5bac927ca9ee438
jbonaiuto <jbonaiuto@gmail.com>
2023-07-07 09:06:08 +0200

    convert from freesurfer to mri coordinates

--- pipeline/pipeline_09_coregister_3dscan_mri.py
+++ pipeline/pipeline_09_coregister_3dscan_mri.py
@@ -1,4 +1,6 @@
+import os
 import os.path as op
+import subprocess
 import sys
 
 import numpy as np
@@ -52,19 +54,20 @@ def run(index, json_file):
         qc_folder = op.join(sess_path, "QC")
         files.make_folder(qc_folder)
 
+        # Get fiducial coordinates in head space
         lpa_str=fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (fiducial_data['session_id'] == session_id), 'LPA_head'].item()
         rpa_str = fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (fiducial_data['session_id'] == session_id), 'RPA_head'].item()
         nas_str = fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (fiducial_data['session_id'] == session_id), 'NAS_head'].item()
-
         lpa = np.array([float(x) for x in lpa_str.split(' ')])*1e-3
         rpa = np.array([float(x) for x in rpa_str.split(' ')])*1e-3
         nas = np.array([float(x) for x in nas_str.split(' ')])*1e-3
 
+        # Get the 3d surface of the face
         dig_face_fname = op.join(raw_path, subject_id, session_id, 'meg', '{}_dig_face.stl'.format(subject_id))
         dig_face = mesh.Mesh.from_file(dig_face_fname)
-
+        # Get all vertices as points
         points = np.around(np.unique(dig_face.vectors.reshape([int(dig_face.vectors.size / 3), 3]), axis=0), 2) * 1e-3
-
+        # Create digital montage for MNE
         dig_montage_fname=op.join(sess_path, '{}-{}-dig.fif'.format(subject_id, session_id))
         dig = mne.channels.make_dig_montage(
             hsp=points,
@@ -73,13 +76,14 @@ def run(index, json_file):
             nasion=nas,
             coord_frame='head'
         )
-        dig.save(dig_montage_fname,
-                 overwrite=True)
+        dig.save(dig_montage_fname, overwrite=True)
 
+        # Create a fake info object for the digital montage
         info = _empty_info(1)
         info["dig"] = read_dig_fif(fname=dig_montage_fname).dig
         info._unlocked = False
 
+        # Plotting options
         plot_kwargs = dict(
             subject=subject_id,
             subjects_dir=subjects_dir,
@@ -92,9 +96,11 @@ def run(index, json_file):
         )
         view_kwargs = dict(azimuth=45, elevation=90, distance=0.6, focalpoint=(0.0, 0.0, 0.0))
 
+        # Create a coregistration
         coreg = Coregistration(info, subject_id, subjects_dir, fiducials='auto', on_defects="ignore")
         coreg._setup_digs()
 
+        # Visualize initial alignment
         align_fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
         screenshot = align_fig.plotter.screenshot()
         fig, ax = plt.subplots(figsize=(10, 10))
@@ -103,9 +109,11 @@ def run(index, json_file):
         fig.tight_layout()
         fig.savefig(op.join(qc_folder, '{}-{}-coreg-initial.png'.format(subject_id, session_id)))
 
+        # Rough fit of fiducials to get rough alignment
         coreg.set_scale_mode('uniform')
         coreg.fit_fiducials(verbose=True)
 
+        # Visualize rough alignment
         align_fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
         screenshot = align_fig.plotter.screenshot()
         fig, ax = plt.subplots(figsize=(10, 10))
@@ -114,7 +122,10 @@ def run(index, json_file):
         fig.tight_layout()
         fig.savefig(op.join(qc_folder, '{}-{}-coreg-fit_fiducials.png'.format(subject_id, session_id)))
 
+        # ICP fit - not fitting fiducial locations, just digitised points
         coreg.fit_icp(n_iterations=50, nasion_weight=0.0,  lpa_weight=0.0, rpa_weight=0.0, verbose=True)
+
+        # Visualize final alignment
         align_fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
         screenshot = align_fig.plotter.screenshot()
         fig, ax = plt.subplots(figsize=(10, 10))
@@ -123,9 +134,11 @@ def run(index, json_file):
         fig.tight_layout()
         fig.savefig(op.join(qc_folder, '{}-{}-coreg-fit_icp.png'.format(subject_id, session_id)))
 
+        # Save transformation
         trans_fname=op.join(sess_path, '{}-{}-trans.fif'.format(subject_id, session_id))
         mne.write_trans(trans_fname, coreg.trans, overwrite=True)
 
+        # Apply inverse transformation to fiducial coordinates to get coordinates in FreeSurfer space
         lpa_t=apply_trans(invert_transform(coreg.trans), lpa)*1e3
         rpa_t = apply_trans(invert_transform(coreg.trans), rpa) * 1e3
         nas_t = apply_trans(invert_transform(coreg.trans), nas) * 1e3
@@ -136,7 +149,26 @@ def run(index, json_file):
         fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (fiducial_data['session_id'] == session_id), 'RPA_FS'] = rpa_t_str
         fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (fiducial_data['session_id'] == session_id), 'NAS_FS'] = nas_t_str
 
-    fiducial_data.to_csv(op.join(proc_path, 'fiducials.csv'))
+        # Read RAS offset from freesurfer volume
+        out=subprocess.check_output(['/usr/local/freesurfer/6.0.0/bin/mri_info', '--cras', os.path.join(subjects_dir,subject_id, 'mri/orig.mgz')])
+        ras_offset = np.array([float(x) for x in out.decode().split(' ')])
+
+        # Convert Freesurfer fiducial coordinates to MRI
+        lpa_t=lpa_t+ras_offset
+        rpa_t=rpa_t+ras_offset
+        nas_t=nas_t+ras_offset
+        lpa_t_str = ' '.join(['{}'.format(x) for x in lpa_t])
+        rpa_t_str = ' '.join(['{}'.format(x) for x in rpa_t])
+        nas_t_str = ' '.join(['{}'.format(x) for x in nas_t])
+        fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (
+                    fiducial_data['session_id'] == session_id), 'LPA_MRI'] = lpa_t_str
+        fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (
+                    fiducial_data['session_id'] == session_id), 'RPA_MRI'] = rpa_t_str
+        fiducial_data.loc[(fiducial_data['subject_id'] == subject_id) & (
+                    fiducial_data['session_id'] == session_id), 'NAS_MRI'] = nas_t_str
+
+
+    fiducial_data.to_csv(op.join(proc_path, 'fiducials.csv'), index=False)
 
 if __name__=='__main__':
     # parsing command line arguments
